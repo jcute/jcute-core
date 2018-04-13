1adbba6101288ede917dbeb2614c5a9cf18bc925
tb_bin@163.com <tb_bin@163.com>
2018-04-13 08:56:00 +0800

    添加order注释

--- src/main/java/com/jcute/core/annotation/Order.java
+++ src/main/java/com/jcute/core/annotation/Order.java
@@ -6,6 +6,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 可定义排序规则,目前在Initial.Destory注解中可用
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE,ElementType.METHOD})
 @Documented
